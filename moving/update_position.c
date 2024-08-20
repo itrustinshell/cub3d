efd4acc26cf30508269d11b35c000be836ffa867
Tron <luigi.argens@gmail.com>
2024-08-20 12:45:30 +0200

    ridotte le fuznioni relative al moving position

--- moving/update_position.c
+++ moving/update_position.c
@@ -1,68 +1,51 @@
 #include "../cub3d.h"
 
 
-
-
-/*this functions receives the movement (up, down, left, right)  of the player (t_move struct), and the reference to
- player position. If the movement is 1 (=HAS_MOVED_UP), then the new position of the
-player is updated.
-This function is also the occasion to update the tile where the player is in.*/
-int move_up(int movement, t_point *player_position, t_c3d *c3d)
+int step_management(t_point *position, t_move move)
 {
-    if (movement)
-    {   
-        if(!is_collision(player_position->x, player_position->y - FOOT_STEP, c3d))
+    if(move.up)
+    {
+        position->y -= FOOT_STEP;
+        return (HAS_MOVED_UP);
+    }
+    else if(move.down)
+    {
+        position->y += FOOT_STEP;
+        return (HAS_MOVED_UP);
+    }
+    else if(move.left)
+    {
+        position->x -= FOOT_STEP;
+        return (HAS_MOVED_UP);
+    }
+    else if(move.right)
+    {
+        position->x += FOOT_STEP;
+        return (HAS_MOVED_UP);
+    }
+    return (DID_NOT_MOVE_UP);
+}
+
+/*this functions save the position of the player in a tmp variable.
+Then works on this last variable.
+It first update the coordinate of this point depending on step_management function.
+Then it checks if the new point is a collision or not.
+if everything was ok, the player position is update with the new valuse.
+This function is also the occasion to update the tile where the player is in.*/
+
+int moving(t_move move, t_point *player_position, t_c3d *c3d)
+{
+    t_point position_to_check;
+    
+    position_to_check = *player_position;
+    if (step_management(&position_to_check, move))
+    {
+        if(!is_collision(position_to_check.x, position_to_check.y, c3d))
         {
-            player_position->y -= FOOT_STEP;
-	        c3d->player.tile = tile_reference(*player_position);
+            *player_position = position_to_check;
+            c3d->player.tile = tile_reference(*player_position);
             return (HAS_MOVED_UP);
         }
-    }
-    return (DID_NOT_MOVE_UP);
-}
-
-
-/*same logic of move_up*/
-int move_down(int movement, t_point *player_position, t_c3d *c3d)
-{
-    if (movement)
-    {
-        if(!is_collision(player_position->x, player_position->y + FOOT_STEP, c3d))
-        {
-            player_position->y += FOOT_STEP;
-	        c3d->player.tile = tile_reference(c3d->player.position);
-            return (HAS_MOVED_UP);
-        }   
-    }
-    return (DID_NOT_MOVE_UP);
-}
-
-/*same logic of move_up*/
-int move_left(int movement, t_point *player_position, t_c3d *c3d)
-{
-    if (movement)
-    {
-        if(!is_collision(player_position->x - FOOT_STEP, player_position->y, c3d))
-        {
-            player_position->x -= FOOT_STEP;
-	        c3d->player.tile = tile_reference(c3d->player.position);
-            return (HAS_MOVED_UP);
-        }  
-    }
-    return (DID_NOT_MOVE_UP);
-}
-
-/*same logic of move_up*/
-int move_right(int movement, t_point *player_position, t_c3d *c3d)
-{
-    if (movement)
-    {
-        if(!is_collision(player_position->x + FOOT_STEP, player_position->y, c3d))
-        {
-            player_position->x += FOOT_STEP;
-	        c3d->player.tile = tile_reference(c3d->player.position);
-            return (HAS_MOVED_UP);
-        }   
     }
     return (DID_NOT_MOVE_UP);
 }
@@ -89,13 +72,7 @@ int update_position(void *param)
       
     does_it_move = 0;
     c3d = (t_c3d *)param;   
-    if(move_up(c3d->player.move.up, &c3d->player.position, c3d))
-        does_it_move = YES;
-    if(move_down(c3d->player.move.down, &c3d->player.position, c3d))
-        does_it_move = YES;
-    if(move_left(c3d->player.move.left, &c3d->player.position, c3d))
-        does_it_move = YES;
-    if(move_right(c3d->player.move.right, &c3d->player.position, c3d))
+    if (moving(c3d->player.move, &c3d->player.position,  c3d))
         does_it_move = YES;
     if (does_it_move == YES)
         stuff_to_draw(c3d);
