a814e60835db94cf555d4a8c088a16607b4fc946
Luigi Pennisi <lpennisi@student.42roma.it>
2025-03-15 17:23:37 +0100

    norminetted

--- srcs/dda/utils/dda_checks.c
+++ srcs/dda/utils/dda_checks.c
@@ -6,130 +6,104 @@
 /*   By: lpennisi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 17:18:50 by lpennisi          #+#    #+#             */
-/*   Updated: 2025/03/15 16:54:18 by lpennisi         ###   ########.fr       */
+/*   Updated: 2025/03/15 17:23:14 by lpennisi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "c3d.h"
-static int	check_ne(char **map_grid, double card_dir, t_point point_to_verify)
-{
-    t_point	p;
-    int		px;
-    int		py;
-    int		map_height;
-    int		map_width;
 
-    p.x = 0;
-    p.y = 0;
-    p = point_to_verify;
-    px = (int)p.x;
-    py = (int)p.y;
-    map_height = ft_strlen(map_grid[0]);
-    map_width = ft_arrlen(map_grid);
-    if (card_dir == NE)
-    {
-        if (py / TILE_SIZE >= map_width || (px - 1) / TILE_SIZE >= map_height || (py + 1) / TILE_SIZE >= map_width || px / TILE_SIZE >= map_height)
-            return (ft_printf("HELLLLOOOOO\n"), FALSE);
-        if (map_grid[py / TILE_SIZE][(px - 1) / TILE_SIZE] == '1' && \
-        map_grid[(py + 1) / TILE_SIZE][px / TILE_SIZE] == '1')
-        {
-            p.x = p.x - 1;
-            p.y = p.y + 1;
-            return (TRUE);
-        }
-    }
-    return (FALSE);
+static int	check_ne(char **map_grid, double card_dir, t_point p)
+{
+	int		h;
+	int		w;
+
+	h = ft_strlen(map_grid[0]);
+	w = ft_arrlen(map_grid);
+	if (card_dir == NE)
+	{
+		if ((int)p.y / TILE_SIZE >= w || ((int)p.x - 1) / TILE_SIZE >= h \
+		|| ((int)p.y + 1) / TILE_SIZE >= w || (int)p.x / TILE_SIZE >= h)
+			return (ft_printf("HELLLLOOOOO\n"), FALSE);
+		if (map_grid[(int)p.y / TILE_SIZE][((int)p.x - 1) / TILE_SIZE] == '1' \
+		&& map_grid[((int)p.y + 1) / TILE_SIZE][(int)p.x / TILE_SIZE] == '1')
+		{
+			p.x = p.x - 1;
+			p.y = p.y + 1;
+			return (TRUE);
+		}
+	}
+	return (FALSE);
 }
 
-static int	check_nw(char **map_grid, double card_dir, t_point point_to_verify)
+static int	check_nw(char **map_grid, double card_dir, t_point p)
 {
-    t_point	p;
-    int		px;
-    int		py;
-    int		map_height;
-    int		map_width;
+	int		h;
+	int		w;
 
-    p.x = 0;
-    p.y = 0;
-    p = point_to_verify;
-    px = (int)p.x;
-    py = (int)p.y;
-    map_height = ft_strlen(map_grid[0]);
-    map_width = ft_arrlen(map_grid);
-    if (card_dir == NW)
-    {
-        if (py / TILE_SIZE >= map_width || (px + 1) / TILE_SIZE >= map_height || (py + 1) / TILE_SIZE >= map_width || px / TILE_SIZE >= map_height)
-            return (ft_printf("HELLLLOOOOO\n"), FALSE);
-        if (map_grid[py / TILE_SIZE][(px + 1) / TILE_SIZE] == '1' && \
-        map_grid[(py + 1) / TILE_SIZE][px / TILE_SIZE] == '1')
-        {
-            p.x = p.x - 1;
-            p.y = p.y + 1;
-            return (TRUE);
-        }
-    }
-    return (FALSE);
+	h = ft_strlen(map_grid[0]);
+	w = ft_arrlen(map_grid);
+	if (card_dir == NW)
+	{
+		if ((int)p.y / TILE_SIZE >= w || ((int)p.x + 1) / TILE_SIZE >= h \
+		|| ((int)p.y + 1) / TILE_SIZE >= w || (int)p.x / TILE_SIZE >= h)
+			return (ft_printf("HELLLLOOOOO\n"), FALSE);
+		if (map_grid[(int)p.y / TILE_SIZE][((int)p.x + 1) / TILE_SIZE] == '1' \
+		&& map_grid[((int)p.y + 1) / TILE_SIZE][(int)p.x / TILE_SIZE] == '1')
+		{
+			p.x = p.x - 1;
+			p.y = p.y + 1;
+			return (TRUE);
+		}
+	}
+	return (FALSE);
 }
 
-static int	check_se(char **map_grid, double card_dir, t_point point_to_verify)
+static int	check_se(char **map_grid, double card_dir, t_point p)
 {
-    t_point	p;
-    int		px;
-    int		py;
-    int		map_height;
-    int		map_width;
+	int		h;
+	int		w;
 
-    p.x = 0;
-    p.y = 0;
-    p = point_to_verify;
-    px = (int)p.x;
-    py = (int)p.y;
-    map_height = ft_strlen(map_grid[0]);
-    map_width = ft_arrlen(map_grid);
-    if (card_dir == SE)
-    {
-        if (py / TILE_SIZE >= map_width || (px - 1) / TILE_SIZE >= map_height || (py - 1) / TILE_SIZE >= map_width || px / TILE_SIZE >= map_height)
-            return (ft_printf("HELLLLOOOOO\n"), FALSE);
-        if (map_grid[py / TILE_SIZE][(px - 1) / TILE_SIZE] == '1' && \
-        map_grid[(py - 1) / TILE_SIZE][px / TILE_SIZE] == '1')
-        {
-            p.x = p.x - 1;
-            p.y = p.y + 1;
-            return (TRUE);
-        }
-    }
-    return (FALSE);
+	h = ft_strlen(map_grid[0]);
+	w = ft_arrlen(map_grid);
+	if (card_dir == SE)
+	{
+		if ((int)p.y / TILE_SIZE >= w || ((int)p.x - 1) / TILE_SIZE >= h \
+		|| ((int)p.y - 1) / TILE_SIZE >= w || (int)p.x / TILE_SIZE >= h)
+			return (ft_printf("HELLLLOOOOO\n"), FALSE);
+		if (map_grid[(int)p.y / TILE_SIZE][((int)p.x - 1) / TILE_SIZE] == '1' \
+		&& map_grid[((int)p.y - 1) / TILE_SIZE][(int)p.x / TILE_SIZE] == '1')
+		{
+			p.x = p.x - 1;
+			p.y = p.y + 1;
+			return (TRUE);
+		}
+	}
+	return (FALSE);
 }
 
-static int	check_sw(char **map_grid, double card_dir, t_point point_to_verify)
+static int	check_sw(char **map_grid, double card_dir, t_point p)
 {
-    t_point	p;
-    int		px;
-    int		py;
-    int		map_height;
-    int		map_width;
+	int		h;
+	int		w;
 
-    p.x = 0;
-    p.y = 0;
-    p = point_to_verify;
-    px = (int)p.x;
-    py = (int)p.y;
-    map_height = ft_strlen(map_grid[0]);
-    map_width = ft_arrlen(map_grid);
-    if (card_dir == SW)
-    {
-        if (py / TILE_SIZE >= map_width || (px + 1) / TILE_SIZE >= map_height || (py - 1) / TILE_SIZE >= map_width || px / TILE_SIZE >= map_height)
-            return (ft_printf("HEAAAAAAOOO\n"), FALSE);
-        if (map_grid[py / TILE_SIZE][(px + 1) / TILE_SIZE] == '1' && \
-        map_grid[(py - 1) / TILE_SIZE][px / TILE_SIZE] == '1')
-        {
-            p.x = p.x - 1;
-            p.y = p.y + 1;
-            return (TRUE);
-        }
-    }
-    return (FALSE);
+	h = ft_strlen(map_grid[0]);
+	w = ft_arrlen(map_grid);
+	if (card_dir == SW)
+	{
+		if ((int)p.y / TILE_SIZE >= w || ((int)p.x + 1) / TILE_SIZE >= h \
+		|| ((int)p.y - 1) / TILE_SIZE >= w || (int)p.x / TILE_SIZE >= h)
+			return (ft_printf("HEAAAAAAOOO\n"), FALSE);
+		if (map_grid[(int)p.y / TILE_SIZE][((int)p.x + 1) / TILE_SIZE] == '1' \
+		&& map_grid[((int)p.y - 1) / TILE_SIZE][(int)p.x / TILE_SIZE] == '1')
+		{
+			p.x = p.x - 1;
+			p.y = p.y + 1;
+			return (TRUE);
+		}
+	}
+	return (FALSE);
 }
+
 int	is_it_passing_between_two_walls(t_ray *ray, char **map_grid, t_point p)
 {
 	double	direction;
